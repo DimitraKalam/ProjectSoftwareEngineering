a5e62c20c45d61c9182ae3a23056e4394acc95f1
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-05-02 12:06:47 +0300

    Update ToDoList.java

--- Project Code/src/ToDoList.java
+++ Project Code/src/ToDoList.java
@@ -3,4 +3,14 @@ import java.util.Vector;
 public class ToDoList
 {
     Vector<ToDoListEntry> EntryVector;
+
+    public ToDoList()
+    {
+        EntryVector = new Vector<>();
+    }
+
+    public void addNewEntry(ToDoListEntry entry)
+    {
+        EntryVector.add(entry);
+    }
 }
