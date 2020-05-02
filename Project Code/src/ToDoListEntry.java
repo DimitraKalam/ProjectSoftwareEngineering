ed65951c5ca368d9021a217e479d0914ed3680a3
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-05-02 11:39:09 +0300

    Project Code

---
+++ Project Code/src/ToDoListEntry.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class ToDoListEntry
+{
+    // The to - do list entry name
+    private String Name;
+
+    // The to - do list entry text
+    private String Text;
+
+    // The date that this entry was last modified
+    private Date LastModificationDate;
+}
