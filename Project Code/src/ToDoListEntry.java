1ccc4ce5e990fe38b700dd524cb25fe4e1820aef
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-05-02 12:06:43 +0300

    Update ToDoListEntry.java

--- Project Code/src/ToDoListEntry.java
+++ Project Code/src/ToDoListEntry.java
@@ -5,8 +5,8 @@ public class ToDoListEntry
     // The to - do list entry name
     private String Name;
 
-    // The to - do list entry text
-    private String Text;
+    // The to - do list entry information
+    private String Info;
 
     // The date that this entry was last modified
     private Date LastModificationDate;
@@ -15,7 +15,7 @@ public class ToDoListEntry
     public ToDoListEntry(String name, String text, Date lastModificationDate)
     {
         Name = name;
-        Text = text;
+        Info = text;
         LastModificationDate = lastModificationDate;
     }
 
@@ -24,13 +24,28 @@ public class ToDoListEntry
         return Name;
     }
 
-    public String getText()
+    public void setName(String name)
     {
-        return Text;
+        Name = name;
+    }
+
+    public String getInfo()
+    {
+        return Info;
+    }
+
+    public void setInfo(String info)
+    {
+        Info = info;
     }
 
     public Date getLastModificationDate()
     {
         return LastModificationDate;
+    }
+
+    public void setLastModificationDate(Date lastModificationDate)
+    {
+        LastModificationDate = lastModificationDate;
     }
 }
