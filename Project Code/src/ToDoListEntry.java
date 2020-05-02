a0a58e8438e07786e7f2e66c08c292b0882917d6
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-05-02 11:48:26 +0300

    Update ToDoListEntry.java

--- Project Code/src/ToDoListEntry.java
+++ Project Code/src/ToDoListEntry.java
@@ -10,4 +10,27 @@ public class ToDoListEntry
 
     // The date that this entry was last modified
     private Date LastModificationDate;
+
+    // Default Constructor
+    public ToDoListEntry(String name, String text, Date lastModificationDate)
+    {
+        Name = name;
+        Text = text;
+        LastModificationDate = lastModificationDate;
+    }
+
+    public String getName()
+    {
+        return Name;
+    }
+
+    public String getText()
+    {
+        return Text;
+    }
+
+    public Date getLastModificationDate()
+    {
+        return LastModificationDate;
+    }
 }
