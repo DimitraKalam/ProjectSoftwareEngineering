6b228a847e6a36d957eef37b59075f1cc2cb2ecb
AlexanDelimi <alex.delimichalis@gmail.com>
2020-05-19 16:08:03 +0300

    added some classes for the profile feature

---
+++ Project Code/src/LogisticsEmployeeFeatures/Economics.java
@@ -0,0 +1,18 @@
+package LogisticsEmployeeFeatures;
+
+public class Economics {
+    private enum Type{
+        Income,
+        Expenses
+    }
+    private enum Source {
+        Subscriptions,
+                Sales,
+                Salaries,
+        Payments
+    }
+public Economics returnRegistration(){return null;}
+public void saveRegistration(){}
+public void deleteRegistration(){}
+public int returnProfit(){return 0;}
+}
