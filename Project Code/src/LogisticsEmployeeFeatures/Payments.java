f2ab7c17c299936da1e66936680855b9e0783766
AlexanDelimi <alex.delimichalis@gmail.com>
2020-05-19 16:10:31 +0300

    Ετοιμες ολες οι κλασεις απο Αλεξ<3

---
+++ Project Code/src/LogisticsEmployeeFeatures/Payments.java
@@ -0,0 +1,8 @@
+package LogisticsEmployeeFeatures;
+
+public class Payments {
+private enum state{Done, Pending};
+public Payments returnPayment(){return null;}
+public Payments searchPayment(){return null;}
+public void savePayment(){}
+}
