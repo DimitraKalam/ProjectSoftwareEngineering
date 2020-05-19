c107488a163ba52490411f6fa9f0082468367d40
AlexanDelimi <alex.delimichalis@gmail.com>
2020-05-19 12:34:20 +0300

    added some classes for the profile feature

---
+++ Project Code/src/ProfileFeatures/Employee.java
@@ -0,0 +1,21 @@
+package ProfileFeatures;
+
+public class Employee {
+    private String name;
+    private String surname;
+    private enum department {
+        none,
+        HR,
+        Logistics,
+        TechnicalSupport,
+        CostumerSupport
+    }
+    private department position;
+
+public boolean belongsToDepartment(department isthismyposition){
+        if(this.position==isthismyposition)
+        { return true;}
+        return false;
+
+}
+}
