1bb5d05d8dc185f44a2c8439460c75e2b732d05b
AlexanDelimi <alex.delimichalis@gmail.com>
2020-05-20 21:45:13 +0300

    Ετοιμες ολες οι κλασεις απο Αλεξ<3

---
+++ Project Code/src/MainScreen/SidebarController.java
@@ -0,0 +1,61 @@
+package MainScreen;
+
+import com.gluonhq.charm.glisten.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+
+public class SidebarController {
+
+    @FXML
+    private Button MyProfileButton;
+
+    @FXML
+    private Button RepositoryButton;
+
+    @FXML
+    private Button DepartmentButton;
+
+    @FXML
+    private Button SearchButton;
+
+    @FXML
+    private TextField SearchBar;
+
+    @FXML
+    private Button BeginSearchButton;
+
+    public SidebarController() {
+    }
+
+    @FXML
+    void Search(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ShowSearch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToMainPage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToMyDepartment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToMyProfile(ActionEvent event) {
+
+    }
+
+}
+
+
+
+// SearchButton.setVisible(true);
