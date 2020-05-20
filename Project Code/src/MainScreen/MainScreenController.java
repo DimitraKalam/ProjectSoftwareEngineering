97a1046f6a7904f5b20c0d92fbe160ed8148b176
AlexanDelimi <alex.delimichalis@gmail.com>
2020-05-20 17:11:11 +0300

    Ετοιμες ολες οι κλασεις απο Αλεξ<3

---
+++ Project Code/src/MainScreen/MainScreenController.java
@@ -0,0 +1,25 @@
+package MainScreen; /**
+ * Sample Skeleton for 'mainscreen.fxml' Controller Class
+ */
+
+import com.gluonhq.charm.glisten.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+
+public class MainScreenController {
+
+    @FXML // fx:id="SearchButton"
+    private Button SearchButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="SearchBar"
+    private TextField SearchBar; // Value injected by FXMLLoader
+
+    @FXML
+    void ShowSearch(ActionEvent event) {
+        SearchButton.setVisible(true);
+
+    }
+
+}
