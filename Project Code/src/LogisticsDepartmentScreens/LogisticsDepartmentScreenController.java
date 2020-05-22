5ef426bffbafff0e27413838d1810eeac9e1ba35
AlexanDelimi <alex.delimichalis@gmail.com>
2020-05-22 15:02:45 +0300

    Δημιουργηθηκαν τα fxml για sidebar, main screen, searchresults,chat και logistics, μενουν essages Profile, economics, payments.

---
+++ Project Code/src/LogisticsDepartmentScreens/LogisticsDepartmentScreenController.java
@@ -0,0 +1,24 @@
+package LogisticsDepartmentScreens;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+public class LogisticsDepartmentScreenController {
+
+        @FXML
+        private Button EconomicsButton;
+
+        @FXML
+        private Button PaymentsButton;
+
+        @FXML
+        void goToEconomics(ActionEvent event) {
+
+        }
+
+        @FXML
+        void goToPayments(ActionEvent event) {
+
+        }
+
+    }
+
