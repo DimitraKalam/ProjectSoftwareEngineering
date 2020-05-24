a312f40e8ca16e6f21aac87e7e05e64f84ee46aa
AlexanDelimi <alex.delimichalis@gmail.com>
2020-05-24 17:20:56 +0300

    Δημιουργηθηκαν fxml, μενουν messages Profile.

---
+++ Project Code/src/LogisticsDepartmentScreens/EconomicsScreenController.java
@@ -0,0 +1,54 @@
+package LogisticsDepartmentScreens;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class EconomicsScreenController {
+
+    @FXML
+    private Button ProfitButton;
+
+    @FXML
+    private TableView<?> EconomicsTable;
+
+    @FXML
+    private TableColumn<?, ?> NameColumn;
+
+    @FXML
+    private TableColumn<?, ?> DateColumn;
+
+    @FXML
+    private TableColumn<?, ?> AmountColumn;
+
+    @FXML
+    private TableColumn<?, ?> CounterpartyColumn;
+
+    @FXML
+    void MakeEditable(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MakeUneditable(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SaveData(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showProfit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sortAlphabetically(ActionEvent event) {
+
+    }
+
+}
---
+++ Project Code/src/LogisticsDepartmentScreens/PaymentsScreenController.java
@@ -0,0 +1,47 @@
+package LogisticsDepartmentScreens;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.fxml.FXML;
+public class PaymentsScreenController {
+
+
+
+        @FXML
+        private TableView<?> EconomicsTable;
+
+        @FXML
+        private TableColumn<?, ?> NameColumn;
+
+        @FXML
+        private TableColumn<?, ?> DateColumn;
+
+        @FXML
+        private TableColumn<?, ?> AmountColumn;
+
+        @FXML
+        private TableColumn<?, ?> CounterpartyColumn;
+
+        @FXML
+        void MakeEditable(ActionEvent event) {
+
+        }
+
+        @FXML
+        void MakeUneditable(ActionEvent event) {
+
+        }
+
+        @FXML
+        void SaveData(ActionEvent event) {
+
+        }
+
+        @FXML
+        void sortAlphabetically(ActionEvent event) {
+
+        }
+
+    }
+
+
