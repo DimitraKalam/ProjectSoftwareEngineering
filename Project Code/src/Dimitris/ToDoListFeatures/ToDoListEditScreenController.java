b50e6fc80c749344ec764ab0cbb83042eb92ae9b
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-05-28 17:47:28 +0300

    Updates

---
+++ Project Code/src/Dimitris/ToDoListFeatures/ToDoListEditScreenController.java
@@ -0,0 +1,15 @@
+package Dimitris.ToDoListFeatures;
+
+/**
+ * Sample Skeleton for 'ToDoListEditScreen.fxml' Controller Class
+ */
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class ToDoListEditScreenController {
+
+    @FXML // fx:id="EntriesTable"
+    private TableView<?> EntriesTable; // Value injected by FXMLLoader
+
+}
