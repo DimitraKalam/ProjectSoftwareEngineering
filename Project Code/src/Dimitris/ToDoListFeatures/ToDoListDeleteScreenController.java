bb3a8af642e819537c7efc4b1fb2ab41be72dfdc
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-05-30 10:24:53 +0300

    Updates

--- Project Code/src/Dimitris/ToDoListFeatures/ToDoListDeleteScreenController.java
+++ Project Code/src/Dimitris/ToDoListFeatures/ToDoListDeleteScreenController.java
@@ -2,12 +2,24 @@ package Dimitris.ToDoListFeatures; /**
  * Sample Skeleton for 'ToDoListDeleteScreen.fxml' Controller Class
  */
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
+
+import java.util.Vector;
 
 public class ToDoListDeleteScreenController {
 
     @FXML // fx:id="Table"
-    private TableView<?> Table; // Value injected by FXMLLoader
+    private TableView<ToDoList> Table; // Value injected by FXMLLoader
 
+    void InitializeData()
+    {
+        Vector<ToDoList> ToDoListsVector = new Vector<>();
+
+        ToDoListsVector.add(new ToDoList( "List"));
+        ToDoListsVector.add(new ToDoList("Λίστα για το Project"));
+
+        Table.setItems(FXCollections.observableList(ToDoListsVector));
+    }
 }
--- Project Code/src/Dimitris/ToDoListFeatures/ToDoListEditScreenController.java
+++ Project Code/src/Dimitris/ToDoListFeatures/ToDoListEditScreenController.java
@@ -4,13 +4,24 @@ package Dimitris.ToDoListFeatures;
  * Sample Skeleton for 'ToDoListEditScreen.fxml' Controller Class
  */
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
+
+import java.util.Vector;
 
 public class ToDoListEditScreenController {
 
     @FXML // fx:id="EntriesTable"
     private TableView<ToDoList> EntriesTable; // Value injected by FXMLLoader
 
+    void InitializeData()
+    {
+        Vector<ToDoList> ToDoListsVector = new Vector<>();
 
+        ToDoListsVector.add(new ToDoList( "List"));
+        ToDoListsVector.add(new ToDoList("Λίστα για το Project"));
+
+        EntriesTable.setItems(FXCollections.observableList(ToDoListsVector));
+    }
 }
