270e43935f55c02cad3f6e5b9ab7e7665777ec62
AlexanDelimi <alex.delimichalis@gmail.com>
2020-05-30 12:16:54 +0300

    Μενει η οθονη μηνυματψν και η οθονη για την δημιουργια νεας ομαδικής.

---
+++ Project Code/src/Alex/ChatScreen/CallScreenController.java
@@ -0,0 +1,29 @@
+package Alex.ChatScreen;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+public class CallScreenController {
+
+
+    public class PleaseProvideControllerClassName {
+
+        @FXML
+        private ImageView MyPhoto;
+
+        @FXML
+        private ImageView OtherPhoto;
+
+        @FXML
+        private Button EndCallButton;
+
+        @FXML
+        void endCall(ActionEvent event) {
+
+        }
+
+    }
+
+}
+
