c2b8def0037245750b5dc8fac36b0acd13c56346
DimitraKalam <58468941+DimitraKalam@users.noreply.github.com>
2020-05-31 14:35:54 +0300

    Add files via upload

---
+++ more java classes/java classes/src/sample/Calendar.java
@@ -0,0 +1,13 @@
+package sample;
+
+import java.util.Date;
+
+public class Calendar {
+    private java.util.Date Date;
+    private java.sql.Time Time;
+    private int Month;
+    private int Entry;
+
+    public void goToPersonal(){}
+    public void goToPublic(){}
+}
---
+++ more java classes/java classes/src/sample/FileRepositoryManagementFeature.java
@@ -0,0 +1,14 @@
+package sample;
+
+import java.util.Date;
+
+public class FileRepositoryManagementFeature {
+    private String Name;
+    private Date DateCreated;
+    private double CodeMetrics;
+    private int ID;
+    private int Quantity;
+    private String Category;
+    private String AuthorName;
+
+}
